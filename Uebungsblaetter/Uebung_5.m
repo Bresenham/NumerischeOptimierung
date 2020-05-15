2c10b2530f8d26f01ee7b5f02c6b1e0d35e40c3c
Bresenham <gaul@hm.edu>
2020-05-15 12:31:46 +0200

    Finished exercise 2 of Uebung5

--- Uebungsblaetter/Uebung_5.m
+++ Uebungsblaetter/Uebung_5.m
@@ -1,22 +1,39 @@
 % Aufgabe 1
+
 % f(x) = 2x1^2 - 4x1x2 + 4x2^2
-% Q1 = [4, -4; -4, 8];
-% q1 = [0; 0];
+f = @(x) 2*x(1).^2 - 4*x(1)*x(2) + 4*x(2).^2;
+grad_f = @(x) [ 4*x(1) - 4*x(2); -4*x(1) + 8*x(2) ];
+
+Q1 = [4, -4; -4, 8];
+q1 = [0; 0];
+x0 = [1; 0.5];
 
 % f(x) = x1^2 + 20x2^2 - 4x1 - 2x2
-% Q2 = [0.5, 0; 0, 10];
-% q2 = [-4; -2];
-% x0 = [-5; 1];
+g = @(x) x(1).^2 + 20*x(2).^2 - 4*x(1) - 2*x(2);
+grad_g = @(x) [ 2*x(1) - 4; 40*x(2) - 2 ];
 
-% ret = ConjugateGradient(Q2, q2, x0);
-% fprintf("RESULT: [%0.6f, %0.6f]\n", ret(1), ret(2));
-% 
-% disp( pcg(Q2, -q2) );
+Q2 = [2, 0; 0, 40];
+q2 = [-4; -2];
+
+ret = ConjugateGradient(Q1, q1, x0);
+fprintf("ConjugateGradient returned x=[%0.6f, %0.6f] with f(x) = %0.4f\n\n", ret(1), ret(2), 0.5 * ret' * Q1 * ret + q1' * ret);
+
+ret = ConjugateGradient(Q2, q2, x0);
+fprintf("ConjugateGradient returned x=[%0.6f, %0.6f] with g(x) = %0.4f\n\n", ret(1), ret(2), 0.5 * ret' * Q2 * ret + q2' * ret);
+
+ret = pcg(Q1, -q1);
+fprintf("pcg returned x=[%0.6f, %0.6f] for f(x)\n\n", ret(1), ret(2));
+
+ret = pcg(Q2, -q2);
+fprintf("pcg returned x=[%0.6f, %0.6f] for g(x)\n\n", ret(1), ret(2));
 
 % Aufgabe 2
-f = @(x) 2*x(1).^2 - 4*x(1)*x(2) + 4*x(2).^2;
-grad = @(x) [4*x(1) - 4*x(2); -4*x(1) + 8*x(2)];
-ret = ConjugateGradientWolfe(f, grad, [1;1]);
+x0 = [1; 0.5];
+ret = ConjugateGradientWolfe(f, grad_f, x0);
+fprintf("ConjugateGradientWolfe returned x=[%0.6f, %0.6f] with f(x) = %0.4f\n\n", ret(1), ret(2), f(ret));
+
+ret = ConjugateGradientWolfe(g, grad_g, x0);
+fprintf("ConjugateGradientWolfe returned x=[%0.6f, %0.6f] with g(x) = %0.4f\n\n", ret(1), ret(2), g(ret));
 
 % Funktion Aufgabe 1
 function ret = ConjugateGradient(Q, q, x0)
@@ -27,6 +44,7 @@ function ret = ConjugateGradient(Q, q, x0)
     d = -grad_f;
 
     while norm(grad_f) > 1e-8 && k < 100
+        
         alpha = - ( grad_f' * d ) / ( d' * (Q*d) );
         x = x + alpha * d;
         grad_f_new = grad_f + alpha*Q*d;
@@ -37,6 +55,7 @@ function ret = ConjugateGradient(Q, q, x0)
         k = k + 1;
     end
     
+    fprintf("ConjugateGradient took %d steps.\n", k);
     ret = x;
 end
 
@@ -44,10 +63,10 @@ end
 % phi_grad = ∇f(x + td)^T * d
 function ret = WolfePowell(phi, phi_grad)
 
-    rho = 0.8;
+    rho = 0.3;
     gamma = 1.5;
     alpha = 1.0;
-    sigma = 1e-3;
+    sigma = 1e-2;
     
     A = @(a) phi(a) <= phi(0) + sigma * a * phi_grad(0);
     W = @(a) phi_grad(a) >= rho * phi_grad(0);
@@ -89,20 +108,22 @@ function ret = ConjugateGradientWolfe(f, grad, x0)
     x = x0;
     d = -grad(x0);
     
-    while norm( grad(x0) ) > 1e-8 && k < 100
-        
-        phi = @(a) f(x + a * d);
-        phi_grad = @(a) grad(x + a * d)' * d;
+    phi = @(a) f(x + a * d);
+    phi_grad = @(a) grad(x + a * d)' * d;
+    
+    while norm( grad(x) ) > 1e-8 && k < 100
 
         alpha = WolfePowell(phi, phi_grad);
         
         x_new = x + alpha * d;
         beta = norm( grad(x_new) )^2 / norm( grad(x) )^2;
         d = -grad(x_new) + beta * d;
+        
         x = x_new;
-        disp(x);
+        
         k = k + 1;
     end
     
+    fprintf("ConjugateGradientWolfe took %d steps.\n", k);
     ret = x;
 end
