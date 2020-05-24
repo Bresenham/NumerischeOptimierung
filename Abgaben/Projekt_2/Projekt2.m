8e3b3dd317adce78c8b96544a04879b75893f5f3
Bresenham <gaul@hm.edu>
2020-05-24 09:50:55 +0200

    Update measurement data

--- Abgaben/Projekt_2/Projekt2.m
+++ Abgaben/Projekt_2/Projekt2.m
@@ -51,27 +51,28 @@ x0 = [0; -1];
 % ret = InverseBFGS(f_rosen, f_rosen_grad, x0);
 % fprintf("InverseBFGS returned x=%s with f_rosen(x)=%0.6f\n", vec2str( ret(end).x ), ret(end).f);
 
-% count = 100;
-% max_dim = 500;
-% all_avgs = zeros(max_dim, 1);
-% 
-% options = optimoptions("fminunc", "OptimalityTolerance", 1e-8, "MaxFunctionEvaluations", 1e+6, "MaxIterations", 1e+6, "HessUpdate", "bfgs", 'Display','off');
-% 
-% for dim = 72:max_dim
-%     sums = zeros(count, 1);
-%     for c = 1:count
-%         x0_rosen = zeros(dim, 1) - ones(dim, 1);
-%         tic;
-%         fminunc(f_rosen_mult, x0_rosen, options);
-%         elapsed = toc;
-%         sums(c) = sums(c) + elapsed;
-%     end
-%     avg = sum(sums) / count;
-%     all_avgs(dim) = avg;
-%     fprintf("DIM = %d\n", dim);
-% end
-% 
-% rosenbrock_dim = 250;
+count = 100;
+max_dim = 500;
+all_avgs = zeros(max_dim, 1);
+
+options = optimoptions("fminunc", "OptimalityTolerance", 1e-8, "MaxFunctionEvaluations", 1e+6, "MaxIterations", 1e+6, "HessUpdate", "bfgs", 'Display','off');
+
+for dim = 72:max_dim
+    sums = zeros(count, 1);
+    for c = 1:count
+        x0_rosen = zeros(dim, 1) - ones(dim, 1);
+        tic;
+        fminunc(f_rosen_mult, x0_rosen, options);
+        elapsed = toc;
+        sums(c) = sums(c) + elapsed;
+    end
+    avg = sum(sums) / count;
+    all_avgs(dim) = avg;
+    fprintf("DIM = %d\n", dim);
+    disp(all_avgs(72:dim));
+end
+
+rosenbrock_dim = 250;
 % x0_rosen = zeros(rosenbrock_dim, 1) - ones(rosenbrock_dim, 1);
 % ret = InverseBFGS(f_rosen_mult, f_rosen_mult_grad, x0_rosen);
 % fprintf("InverseBFGS returned x=%s with f_rosen_mult(x)=%0.6f\n", vec2str( ret(end).x ), ret(end).f);
@@ -135,8 +136,8 @@ fprintf("--------------------AUFGABE 9--------------------\n");
 
 f_lq_sum = @(x) sum( f_resid(x, f_xdata, f_ydata).^2 );
 f_lq_grad = @(x) 2 * f_jacobi(x, f_xdata, f_ydata)' * f_resid(x, f_xdata, f_ydata);
-ret = InverseBFGS(f_lq_sum, f_lq_grad, [0.5; 1]);
-disp(ret);
+% ret = InverseBFGS(f_lq_sum, f_lq_grad, [0.5; 1]);
+% disp(ret);
 
 g_lq_sum = @(x) sum( g_resid(x, g_xdata, g_ydata).^2 );
 g_lq_grad = @(x) 2 * g_jacobi(x, g_xdata, g_ydata)' * g_resid(x, g_xdata, g_ydata);
